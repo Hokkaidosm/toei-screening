1f2d95b02544f49ecd13b5dc5de653295d7eff32
Hokkaidosm <64832302+Hokkaidosm@users.noreply.github.com>
2021-08-21 00:43:09 +0900

    起動するところまで

---
+++ src/main/java/net/hokkaidosm/precure/toei_screening/Main.java
@@ -0,0 +1,10 @@
+package net.hokkaidosm.precure.toei_screening;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Test");
+	}
+
+}
