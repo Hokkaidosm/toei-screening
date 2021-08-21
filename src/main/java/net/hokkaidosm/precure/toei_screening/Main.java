7dcf17ac7f4784830b1c0dc95470455c81c0494a
Hokkaidosm <64832302+Hokkaidosm@users.noreply.github.com>
2021-08-21 17:11:32 +0900

    Springを使うようにする

--- src/main/java/net/hokkaidosm/precure/toei_screening/Main.java
+++ src/main/java/net/hokkaidosm/precure/toei_screening/Main.java
@@ -1,9 +1,15 @@
 package net.hokkaidosm.precure.toei_screening;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
 		System.out.println("Test");
 	}
 
